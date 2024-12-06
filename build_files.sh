bcd33eef1b7dc1285829407b19c02dfc0813e7ae
Mohammed Afnas <afnaskkvlr@gmail.com>
2024-12-06 15:54:24 +0530

    Add build_files.sh and update vercel.json

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,11 @@
+ 
+
 #!/bin/bash
+python -m venv myenv
+source myenv/bin/activate
 pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 mkdir -p .vercel/output/static
 cp -r staticfiles/ .vercel/output/static/
-python3 manage.py makemigrations
-python3 manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
